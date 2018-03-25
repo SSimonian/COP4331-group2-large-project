8284dce1d99df95c02ef1a757ed6a1bd5cd94840
Armand Alvarez <aalvarez97@knights.ucf.edu>
2018-03-25 16:12:49 -0400

    Modified to reflect ERD.

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     user_name: String,
-    password: String
+    password: String,
+    last_login: Date,
+    public_key: String
 });
 
 module.exports = mongoose.model('User', userSchema);
