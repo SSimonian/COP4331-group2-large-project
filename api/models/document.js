a6ff5a480717b9938770e6284591f7f40abe902d
Anthony Jackson <35667475+aJackson1996@users.noreply.github.com>
2018-03-25 17:47:19 -0400

    fixed trailing whitespace

--- api/models/document.js
+++ api/models/document.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const documentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-	nickname: String,
+    nickname: String,
     ciphertext: String,
     expire_time: Date,
     user_id: String,
