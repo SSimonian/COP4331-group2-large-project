73fd2ed8ad3bd3168015a4caef65ead655cc2451
Armand Alvarez <aalvarez97@knights.ucf.edu>
2018-03-25 16:11:53 -0400

    Modified to reflect ERD.

--- api/models/document.js
+++ api/models/document.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const documentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    content: String
+    nickname: String,
+    ciphertext: String,
+    expire_time: Date,
+    user_id: String,
+    recipient_id: String
 });
 
 module.exports = mongoose.model('Document', documentSchema);
