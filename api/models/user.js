026080a44a679065616ba2c2c11e5cb66d672391
MPoulsen <mjpoulsen@users.noreply.github.com>
2018-04-08 19:04:36 -0400

    Modified schema to include required and default tags.

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
+// Number of milliseconds within a year = 31556952000
+
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user_name: String,
-    password: String,
+    user_name: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     last_login: Date,
-    public_key: String
+    public_key: {type: String, required: true},
+    freq: {type: Number, default: 31556952000}
 });
 
 module.exports = mongoose.model('User', userSchema);
