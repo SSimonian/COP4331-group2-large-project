4d2e4c8a9bac08bc1a6d0aa6b1bd816408245e38
MPoulsen <mjpoulsen@users.noreply.github.com>
2018-04-08 19:03:57 -0400

    Modified schema to create a relationship between documents and users and added required tags.

--- api/models/document.js
+++ api/models/document.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const documentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    nickname: String,
-    ciphertext: String,
+    nickname: {type: String, required: true},
+    ciphertext: {type: String, required: true},
     expire_time: Date,
-    user_id: String,
-    recipient_id: String
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    recipient_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 module.exports = mongoose.model('Document', documentSchema);
