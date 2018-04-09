b7f64c78947d658ef12da6c4169a0964e03853c5
MPoulsen <mjpoulsen@users.noreply.github.com>
2018-04-09 12:15:36 -0400

    Commented superfluous lines.

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -11,15 +11,15 @@ const User = require('../models/user');
         req.userSession.reset();
         res.redirect('/login');
       } else {
-        // Create JSON string.
-        var json = '{ "_id": "' + user._id + '", "user_name": "' + user.user_name + '", "last_login": ' + user.last_login.getTime() + ' }';
-        // Parse JSON string.
-        var user_details = JSON.parse(json);
-
-        // Set session to user.
-        res.locals.user = user;
-
-        console.log("Profile.js: " + user_details + " stringify: " + JSON.stringify(user_details));
+        // // Create JSON string.
+        // var json = '{ "_id": "' + user._id + '", "user_name": "' + user.user_name + '", "last_login": ' + user.last_login.getTime() + ' }';
+        // // Parse JSON string.
+        // var user_details = JSON.parse(json);
+        //
+        // // Set session to user.
+        // res.locals.user = user;
+        //
+        // console.log("Profile.js: " + user_details + " stringify: " + JSON.stringify(user_details));
         // render the dashboard page
         res.render('profile', {
           _id: user._id,
